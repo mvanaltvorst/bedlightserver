227562dc6ea7592134a2045e4b94ea86cbc5633e
Maurits van Altvorst <mvanaltvorst@icloud.com>
2019-08-10 12:33:50 +0200

    Add production build

---
+++ bedlightwebserver/build/precache-manifest.8872b2c88a2be4d1643d7555c7b5acfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345df7a148580fbf9b070c74de164679",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "d3e67fcc29892f2be345",
+    "url": "/static/static/css/main.a14dcb38.chunk.css"
+  },
+  {
+    "revision": "6fccb0678ec0cb297c1b",
+    "url": "/static/static/js/2.796401f2.chunk.js"
+  },
+  {
+    "revision": "d3e67fcc29892f2be345",
+    "url": "/static/static/js/main.1e98085c.chunk.js"
+  },
+  {
+    "revision": "f70fbb4b460385a1418a",
+    "url": "/static/static/js/runtime~main.647f6be3.js"
+  }
+]);
